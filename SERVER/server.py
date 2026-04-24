70659e313c8dcecf34a9491226c33f4122187547
Elena Anfilofeva <elenanfilofeva@gmail.com>
2026-04-24 11:57:20 +0200

    Modifications formatage trame serveur

--- SERVER/server.py
+++ SERVER/server.py
@@ -1,100 +1,61 @@
-# Program to control passerelle between Android application
-# and micro-controller through USB tty
-import time
-import argparse
-import signal
-import sys
-import socket
-import socketserver
-import serial
-import threading
+import struct
 
-HOST           = "0.0.0.0"
-UDP_PORT       = 10000
-MICRO_COMMANDS = ["TL" , "LT"]
-FILENAME        = "values.txt"
-LAST_VALUE      = ""
+from interface.interface_save import InterfaceSave
+from interface.interface_encodage import InterfaceEncodage
+from adapter import SerialAdapter
 
-class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        current_thread = threading.current_thread()
-        print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
-        if data != "":
-                        if data in MICRO_COMMANDS: # Send message through UART
-                                sendUARTMessage(data)
+class ServerIot:
+    def __init__(self, adapter, encodage, storage, mon_adresse: str):
+        self.adapter = adapter
+        self.encodage = encodage
+        self.storage = storage
+        self.mon_adresse = str(mon_adresse)
+       
+    def start(self):
+        print("Starting IoT Server...")
+        self.run_serial_loop()
+        
+    def run_serial_loop(self):
+        buffer_global = b""  # Notre mémoire tampon
+        
+        while True:
+            try:
+                raw_chunk = self.adapter.read_raw()
+                
+                if raw_chunk:
+                    buffer_global += raw_chunk
+                    
+                    # extract_frames utilisera automatiquement la nouvelle taille de 25 octets
+                    # si vous avez bien mis à jour MaTrame dans binary_encodage.py
+                    trames_completes, buffer_global = self.encodage.extract_frames(buffer_global)
+                    
+                    for trame in trames_completes:
+                        adresse_recue = self.encodage.extract_address(trame)
+                        
+                        if adresse_recue in (self.mon_adresse, "0"):
+                            # --- MODIFICATION ICI : Format '<B3sfffffB' pour 5 floats ---
+                            # B (1), 3s (3), f (4), f (4), f (4), f (4), f (4), B (1) = 25 octets
+                            # Mettre à jour le unpack pour 5s (string de 5)
+                            brut = struct.unpack('<B5sfffffB', trame)
+                            tag_brut = brut[1].decode('utf-8')
+                            
+                            # On récupère les 5 valeurs brutes
+                            f1_b, f2_b, f3_b, f4_b, f5_b = brut[2], brut[3], brut[4], brut[5], brut[6]
                                 
-                        elif data == "getValues()": # Sent last value received from micro-controller
-                                socket.sendto(LAST_VALUE, self.client_address) 
-                                # TODO: Create last_values_received as global variable      
+                            
+                            # Affichage brut avec les 5 capteurs (T, L, H, P, U)
+                            print(f"\n[RADIO BRUT] {tag_brut} | T:{f1_b:.2f} L:{f2_b:.2f} H:{f3_b:.2f} P:{f4_b:.2f} U:{f5_b:.2f}")
+
+                            # --- AFFICHAGE DU FORMAT DÉCODÉ (Utilise votre Model à 5 valeurs) ---
+                            model = self.encodage.decode(trame)
+                            print(f"[SERVEUR OK] {model}")
+                            self.storage.save_data(model)
                         else:
-                                print("Unknown message: ",data)
+                            print(f"[-] Ignoré (Adresse {adresse_recue} inconnue)")
 
-class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
-    pass
-
-
-# send serial message 
-SERIALPORT = "/dev/ttyUSB0"
-BAUDRATE = 115200
-ser = serial.Serial()
-
-def initUART():        
-        # ser = serial.Serial(SERIALPORT, BAUDRATE)
-        ser.port=SERIALPORT
-        ser.baudrate=BAUDRATE
-        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-        ser.parity = serial.PARITY_NONE #set parity check: no parity
-        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-        ser.timeout = None          #block read
-
-        # ser.timeout = 0             #non-block read
-        # ser.timeout = 2              #timeout block read
-        ser.xonxoff = False     #disable software flow control
-        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-        #ser.writeTimeout = 0     #timeout for write
-        print('Starting Up Serial Monitor')
-        try:
-                ser.open()
-        except serial.SerialException:
-                print("Serial {} port not available".format(SERIALPORT))
-                exit()
-
-
-
-def sendUARTMessage(msg):
-    ser.write(msg.encode())
-    print("Message <" + msg + "> sent to micro-controller." )
-
-
-# Main program logic follows:
-if __name__ == '__main__':
-        initUART()
-        f= open(FILENAME,"a")
-        print ('Press Ctrl-C to quit.')
-
-        server = ThreadedUDPServer((HOST, UDP_PORT), ThreadedUDPRequestHandler)
-
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-
-        try:
-                server_thread.start()
-                print("Server started at {} port {}".format(HOST, UDP_PORT))
-                while ser.isOpen() : 
-                        # time.sleep(100)
-                        if (ser.inWaiting() > 0): # if incoming bytes are waiting 
-                                data_bytes = ser.read(ser.inWaiting())
-                                data_str = data_bytes.decode()
-                                f.write(data_str)
-                                LAST_VALUE = data_str
-                                print(data_str)
-        except (KeyboardInterrupt, SystemExit):
-                server.shutdown()
-                server.server_close()
-                f.close()
-                ser.close()
-                exit()
+            except KeyboardInterrupt:
+                self.stop()
+                break
+                
+    def stop(self):
+        self.adapter.close_connection()
